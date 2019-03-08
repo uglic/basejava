0cfe8930d3a3079a5517b3b324332a1bc6e99772
Stepan Shcherbakov <me@uglic.ru>
2019-03-08 18:15:43 +0300

    Class tested

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    /**
+     * Storage of resume
+     */
+    private final Resume[] storage = new Resume[10000];
 
     /**
      * Count of saved elements
@@ -17,7 +22,7 @@ public class ArrayStorage {
      */
     void clear() {
 
-        // this check may be needed for correctness of start index for clear
+        // check correctness of start index for clear
         if (size > storage.length) {
             size = storage.length;
         }
@@ -30,13 +35,18 @@ public class ArrayStorage {
 
     /**
      * Add Resume item to the tail of storage
-     * Throw an IndexOutOfBoundsException exception in case
-     * of storage is already full
+     * Throw an IndexOutOfBoundsException exception
+     * in a case of the storage is already full.
+     * Null values not allowed, throw an NullPointerException in that case
      *
      * @param r Resume object to add to storage
+     * @throws IndexOutOfBoundsException Storage is already full
+     * @throws NullPointerException      Try to add null as Resume
      */
     void save(Resume r) {
-
+        if (r == null) {
+            throw new NullPointerException();
+        }
         // Throw exception if final size more than allowed
         if (size > storage.length - 1) {
             throw new IndexOutOfBoundsException();
@@ -47,7 +57,7 @@ public class ArrayStorage {
     /**
      * Search for Resume by it's uuid
      * By conditions of this task storage is unsorted
-     * so full search must be done
+     * so full search must be done.
      * Empty string or null instead of String object are allowed
      * Return first founded Resume if several of objects have
      * the same uuid.
@@ -65,29 +75,60 @@ public class ArrayStorage {
     }
 
     /**
-     * Delete first founded Resume from storage and return
-     * Throw an Exception if Resume with uuid was not found
+     * Delete first founded Resume from storage and return.
+     * Throw an IndexOutOfBoundsException exception
+     * if Resume with the uuid was not found.
+     * TODO: may be set return type as Resume and return deleted Resume or null
      *
      * @param uuid String representation of Resume we are looking for
+     * @throws IndexOutOfBoundsException if resume with the uuid was not found
      */
     void delete(String uuid) {
 
-        // Look for item
         for (int i = 0; i < size; i++) {
             if (storage[i] != null && storage[i].toString().equals(uuid)) {
                 storage[i] = null;
+
+                // move right part of array to the right
+
+                // Version 1: before IDEA code analyze
+                /*
+                for (int right = i + 1; right < size; right++) {
+                    storage[right - 1] = storage[right];
+                }
+                */
+
+                // Version 2: after IDEA code analyze
+                // Need test which version is faster
+                System.arraycopy(storage, i + 1, storage, i, size - i - 1);
+
+                // clear double reference to the same Resume
+                storage[size - 1] = null;
+                size--;
+                return;
             }
         }
-
+        throw new IndexOutOfBoundsException();
     }
 
     /**
+     * Return all saved resumed
+     *
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        if (size == 0) {
+            return new Resume[0];
+        } else {
+            return Arrays.copyOfRange(storage, 0, size);
+        }
     }
 
+    /**
+     * Count of saved elements in storage
+     *
+     * @return Count of saved elements in storage
+     */
     int size() {
         return this.size;
     }
