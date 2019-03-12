ee9fcef983e8d47824a282b562ab3e866c9036b8
Stepan Shcherbakov <me@uglic.ru>
2019-03-12 21:41:12 +0300

    Lesson #04: correction: added toString() throught reflection

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -6,13 +6,15 @@ import java.lang.reflect.Field;
 
 public class MainReflection {
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws ReflectiveOperationException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
+        System.out.println("call via reflection:");
+        r.getClass().getMethod("toString").invoke(r);
         // TODO : invoke r.toString via reflection
         System.out.println(r);
     }
