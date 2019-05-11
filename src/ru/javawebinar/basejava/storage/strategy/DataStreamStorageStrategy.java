28145d97708a16e73b7b699f13db8aae00b7a684
Stepan Shcherbakov <me@uglic.ru>
2019-05-12 00:23:21 +0300

    Lesson#10: intermediate DataStream read to functional form

--- src/ru/javawebinar/basejava/storage/strategy/DataStreamStorageStrategy.java
+++ src/ru/javawebinar/basejava/storage/strategy/DataStreamStorageStrategy.java
@@ -5,10 +5,8 @@ import ru.javawebinar.basejava.model.*;
 
 import java.io.*;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Supplier;
 
 public class DataStreamStorageStrategy implements IOStorageStrategy {
     @Override
@@ -17,15 +15,15 @@ public class DataStreamStorageStrategy implements IOStorageStrategy {
             dataOutputStream.writeUTF(resume.getUuid());
             dataOutputStream.writeUTF(resume.getFullName());
             Map<ContactTypes, Contact> contacts = resume.getContacts();
-            dataOutputStream.writeInt(contacts.size());
-            forEachDataOutputStream(contacts.entrySet(), contactEntry -> {
+            forEachConsume(Arrays.asList(contacts.size()), size -> dataOutputStream.writeInt(size));
+            forEachConsume(contacts.entrySet(), contactEntry -> {
                 dataOutputStream.writeUTF(contactEntry.getKey().name());
                 dataOutputStream.writeUTF(contactEntry.getValue().getName());
                 dataOutputStream.writeUTF(contactEntry.getValue().getUrl());
             });
             Map<SectionTypes, AbstractSection> sections = resume.getSections();
-            dataOutputStream.writeInt(sections.size());
-            forEachDataOutputStream(sections.entrySet(), abstractSectionEntry -> {
+            forEachConsume(Arrays.asList(sections.size()), size -> dataOutputStream.writeInt(size));
+            forEachConsume(sections.entrySet(), abstractSectionEntry -> {
                 AbstractSection section = abstractSectionEntry.getValue();
                 dataOutputStream.writeUTF(abstractSectionEntry.getKey().name());
                 switch (abstractSectionEntry.getKey()) {
@@ -35,17 +33,17 @@ public class DataStreamStorageStrategy implements IOStorageStrategy {
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        dataOutputStream.writeInt(((BulletedTextListSection) section).getItems().size());
-                        forEachDataOutputStream(((BulletedTextListSection) section).getItems(), dataOutputStream::writeUTF);
+                        forEachConsume(Arrays.asList(((BulletedTextListSection) section).getItems().size()), size -> dataOutputStream.writeInt(size));
+                        forEachConsume(((BulletedTextListSection) section).getItems(), dataOutputStream::writeUTF);
                         break;
                     case EXPERIENCE:
                     case EDUCATION:
-                        dataOutputStream.writeInt(((OrganizationSection) section).getOrganizations().size());
-                        forEachDataOutputStream(((OrganizationSection) section).getOrganizations(), organization -> {
+                        forEachConsume(Arrays.asList(((OrganizationSection) section).getOrganizations().size()), size -> dataOutputStream.writeInt(size));
+                        forEachConsume(((OrganizationSection) section).getOrganizations(), organization -> {
                             dataOutputStream.writeUTF(organization.getContact().getName());
                             dataOutputStream.writeUTF(organization.getContact().getUrl());
                             dataOutputStream.writeInt(organization.getHistory().size());
-                            forEachDataOutputStream(organization.getHistory(), history -> {
+                            forEachConsume(organization.getHistory(), history -> {
                                 dataOutputStream.writeUTF(history.getStartDate().toString());
                                 dataOutputStream.writeUTF(history.getEndDate().toString());
                                 dataOutputStream.writeUTF(history.getTitle());
@@ -89,23 +87,49 @@ public class DataStreamStorageStrategy implements IOStorageStrategy {
                         break;
                     case EXPERIENCE:
                     case EDUCATION:
-                        List<Organization> organizations = new ArrayList<>();
-                        int organizationsSize = dataInputStream.readInt();
-                        for (int organizationIndex = 0; organizationIndex < organizationsSize; organizationIndex++) {
-                            Contact contact = new Contact(dataInputStream.readUTF(), dataInputStream.readUTF());
-                            List<Organization.Position> history = new ArrayList<>();
-                            int historySize = dataInputStream.readInt();
-                            for (int historyIndex = 0; historyIndex < historySize; historyIndex++) {
-                                Organization.Position position = new Organization.Position(
-                                        LocalDate.parse(dataInputStream.readUTF()),
-                                        LocalDate.parse(dataInputStream.readUTF()),
-                                        dataInputStream.readUTF(),
-                                        dataInputStream.readUTF()
-                                );
-                                history.add(position);
-                            }
-                            organizations.add(new Organization(contact, history));
-                        }
+                        List<Organization> organizations = getListIOException(
+                                ArrayList::new,
+                                () -> new Organization(
+                                        getListIOException(ArrayList::new, () -> new Contact(dataInputStream.readUTF(), dataInputStream.readUTF()), 1).get(0),
+                                        getListIOException(
+                                                ArrayList::new,
+                                                () -> new Organization.Position(
+                                                        LocalDate.parse(dataInputStream.readUTF()),
+                                                        LocalDate.parse(dataInputStream.readUTF()),
+                                                        dataInputStream.readUTF(),
+                                                        dataInputStream.readUTF()),
+                                                getListIOException(ArrayList::new, dataInputStream::readInt, 1).get(0))
+                                ),
+                                getListIOException(ArrayList::new, dataInputStream::readInt, 1).get(0));
+
+
+//
+//
+//
+//
+//
+//
+//
+//
+//                        //----------
+//                        int organizationsSize = dataInputStream.readInt();
+//                        List<Organization> organizations = new ArrayList<>();
+//                        for (int organizationIndex = 0; organizationIndex < organizationsSize; organizationIndex++) {
+//                            Contact contact = new Contact(dataInputStream.readUTF(), dataInputStream.readUTF());
+//                            organizations.add(
+//                                    new Organization(
+//                                            contact,
+//                                            getListIOException(
+//                                                    ArrayList::new,
+//                                                    () -> new Organization.Position(
+//                                                            LocalDate.parse(dataInputStream.readUTF()),
+//                                                            LocalDate.parse(dataInputStream.readUTF()),
+//                                                            dataInputStream.readUTF(),
+//                                                            dataInputStream.readUTF()),
+//                                                    getListIOException(ArrayList::new, dataInputStream::readInt, 1).get(0))
+//                                    )
+//                            );
+//                        }
                         section = new OrganizationSection(organizations);
                         break;
                     default:
@@ -118,13 +142,26 @@ public class DataStreamStorageStrategy implements IOStorageStrategy {
     }
 
     @FunctionalInterface
-    private interface DataOutputConsumer<T> {
+    private interface IOExceptionConsumer<T> {
         void apply(T t) throws IOException;
     }
 
-    private static <T> void forEachDataOutputStream(Collection<? extends T> collection, DataOutputConsumer<T> action) throws IOException {
+    @FunctionalInterface
+    private interface IOExceptionSupplier<T> {
+        T get() throws IOException;
+    }
+
+    private static <T> void forEachConsume(Collection<? extends T> collection, IOExceptionConsumer<T> action) throws IOException {
         for (T t : collection) {
             action.apply(t);
         }
+    }
+
+    private static <T> List<T> getListIOException(Supplier<? extends List<T>> collectionSupplier, IOExceptionSupplier<T> element, int size) throws IOException {
+        List<T> collection = collectionSupplier.get();
+        for (int i = 0; i < size; i++) {
+            collection.add(element.get());
+        }
+        return collection;
     }
 }
