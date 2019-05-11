30a6a8826458bba0016daa7fc4f74e60e334e1db
Stepan Shcherbakov <me@uglic.ru>
2019-05-12 01:08:22 +0300

    Lesson#10: DataStream read to functional form - version 1

--- src/ru/javawebinar/basejava/storage/strategy/DataStreamStorageStrategy.java
+++ src/ru/javawebinar/basejava/storage/strategy/DataStreamStorageStrategy.java
@@ -15,14 +15,14 @@ public class DataStreamStorageStrategy implements IOStorageStrategy {
             dataOutputStream.writeUTF(resume.getUuid());
             dataOutputStream.writeUTF(resume.getFullName());
             Map<ContactTypes, Contact> contacts = resume.getContacts();
-            forEachConsume(Arrays.asList(contacts.size()), size -> dataOutputStream.writeInt(size));
+            forEachConsume(Arrays.asList(contacts.size()), dataOutputStream::writeInt);
             forEachConsume(contacts.entrySet(), contactEntry -> {
                 dataOutputStream.writeUTF(contactEntry.getKey().name());
                 dataOutputStream.writeUTF(contactEntry.getValue().getName());
                 dataOutputStream.writeUTF(contactEntry.getValue().getUrl());
             });
             Map<SectionTypes, AbstractSection> sections = resume.getSections();
-            forEachConsume(Arrays.asList(sections.size()), size -> dataOutputStream.writeInt(size));
+            forEachConsume(Arrays.asList(sections.size()), dataOutputStream::writeInt);
             forEachConsume(sections.entrySet(), abstractSectionEntry -> {
                 AbstractSection section = abstractSectionEntry.getValue();
                 dataOutputStream.writeUTF(abstractSectionEntry.getKey().name());
@@ -33,12 +33,12 @@ public class DataStreamStorageStrategy implements IOStorageStrategy {
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        forEachConsume(Arrays.asList(((BulletedTextListSection) section).getItems().size()), size -> dataOutputStream.writeInt(size));
+                        forEachConsume(Arrays.asList(((BulletedTextListSection) section).getItems().size()), dataOutputStream::writeInt);
                         forEachConsume(((BulletedTextListSection) section).getItems(), dataOutputStream::writeUTF);
                         break;
                     case EXPERIENCE:
                     case EDUCATION:
-                        forEachConsume(Arrays.asList(((OrganizationSection) section).getOrganizations().size()), size -> dataOutputStream.writeInt(size));
+                        forEachConsume(Arrays.asList(((OrganizationSection) section).getOrganizations().size()), dataOutputStream::writeInt);
                         forEachConsume(((OrganizationSection) section).getOrganizations(), organization -> {
                             dataOutputStream.writeUTF(organization.getContact().getName());
                             dataOutputStream.writeUTF(organization.getContact().getUrl());
@@ -61,82 +61,50 @@ public class DataStreamStorageStrategy implements IOStorageStrategy {
     public Resume read(InputStream inputStream) throws IOException {
         try (DataInputStream dataInputStream = new DataInputStream(inputStream)) {
             Resume resume = new Resume(dataInputStream.readUTF(), dataInputStream.readUTF());
-            int contactsSize = dataInputStream.readInt();
-            for (int contactIndex = 0; contactIndex < contactsSize; contactIndex++) {
-                ContactTypes contactType = ContactTypes.valueOf(dataInputStream.readUTF());
-                Contact contact = new Contact(dataInputStream.readUTF(), dataInputStream.readUTF());
-                resume.addContact(contactType, contact);
-            }
-            int sectionsSize = dataInputStream.readInt();
-            for (int sectionIndex = 0; sectionIndex < sectionsSize; sectionIndex++) {
-                SectionTypes sectionType = SectionTypes.valueOf(dataInputStream.readUTF());
-                AbstractSection section;
-                switch (sectionType) {
-                    case OBJECTIVE:
-                    case PERSONAL:
-                        section = new SimpleTextSection(dataInputStream.readUTF());
-                        break;
-                    case ACHIEVEMENT:
-                    case QUALIFICATIONS:
-                        List<String> bulletedItems = new ArrayList<>();
-                        int bulletedItemsSize = dataInputStream.readInt();
-                        for (int bulletedItemIndex = 0; bulletedItemIndex < bulletedItemsSize; bulletedItemIndex++) {
-                            bulletedItems.add(dataInputStream.readUTF());
+
+            addContacts(resume,
+                    () -> ContactTypes.valueOf(dataInputStream.readUTF()),
+                    () -> new Contact(dataInputStream.readUTF(), dataInputStream.readUTF()),
+                    getListIOException(ArrayList::new, dataInputStream::readInt, 1).get(0));
+
+            addSections(resume,
+                    () -> SectionTypes.valueOf(dataInputStream.readUTF()),
+                    (sectionType) -> {
+                        switch (sectionType) {
+                            case OBJECTIVE:
+                            case PERSONAL:
+                                return new SimpleTextSection(
+                                        getListIOException(ArrayList::new,
+                                                dataInputStream::readUTF, 1).get(0));
+                            case ACHIEVEMENT:
+                            case QUALIFICATIONS:
+                                return new BulletedTextListSection(
+                                        getListIOException(ArrayList::new,
+                                                dataInputStream::readUTF,
+                                                getListIOException(ArrayList::new,
+                                                        dataInputStream::readInt, 1).get(0)));
+                            case EXPERIENCE:
+                            case EDUCATION:
+                                return new OrganizationSection(
+                                        getListIOException(ArrayList::new,
+                                                () -> new Organization(
+                                                        getListIOException(ArrayList::new,
+                                                                () -> new Contact(dataInputStream.readUTF(), dataInputStream.readUTF()), 1).get(0),
+                                                        getListIOException(ArrayList::new,
+                                                                () -> new Organization.Position(
+                                                                        LocalDate.parse(dataInputStream.readUTF()),
+                                                                        LocalDate.parse(dataInputStream.readUTF()),
+                                                                        dataInputStream.readUTF(),
+                                                                        dataInputStream.readUTF()),
+                                                                getListIOException(ArrayList::new,
+                                                                        dataInputStream::readInt, 1).get(0))
+                                                ),
+                                                getListIOException(ArrayList::new, dataInputStream::readInt, 1).get(0)));
+                            default:
+                                throw new StorageException("Unknown section: " + sectionType);
                         }
-                        section = new BulletedTextListSection(bulletedItems);
-                        break;
-                    case EXPERIENCE:
-                    case EDUCATION:
-                        List<Organization> organizations = getListIOException(
-                                ArrayList::new,
-                                () -> new Organization(
-                                        getListIOException(ArrayList::new, () -> new Contact(dataInputStream.readUTF(), dataInputStream.readUTF()), 1).get(0),
-                                        getListIOException(
-                                                ArrayList::new,
-                                                () -> new Organization.Position(
-                                                        LocalDate.parse(dataInputStream.readUTF()),
-                                                        LocalDate.parse(dataInputStream.readUTF()),
-                                                        dataInputStream.readUTF(),
-                                                        dataInputStream.readUTF()),
-                                                getListIOException(ArrayList::new, dataInputStream::readInt, 1).get(0))
-                                ),
-                                getListIOException(ArrayList::new, dataInputStream::readInt, 1).get(0));
-
-
-//
-//
-//
-//
-//
-//
-//
-//
-//                        //----------
-//                        int organizationsSize = dataInputStream.readInt();
-//                        List<Organization> organizations = new ArrayList<>();
-//                        for (int organizationIndex = 0; organizationIndex < organizationsSize; organizationIndex++) {
-//                            Contact contact = new Contact(dataInputStream.readUTF(), dataInputStream.readUTF());
-//                            organizations.add(
-//                                    new Organization(
-//                                            contact,
-//                                            getListIOException(
-//                                                    ArrayList::new,
-//                                                    () -> new Organization.Position(
-//                                                            LocalDate.parse(dataInputStream.readUTF()),
-//                                                            LocalDate.parse(dataInputStream.readUTF()),
-//                                                            dataInputStream.readUTF(),
-//                                                            dataInputStream.readUTF()),
-//                                                    getListIOException(ArrayList::new, dataInputStream::readInt, 1).get(0))
-//                                    )
-//                            );
-//                        }
-                        section = new OrganizationSection(organizations);
-                        break;
-                    default:
-                        throw new StorageException("Unknown section: " + sectionType);
-                }
-                resume.addSection(sectionType, section);
-            }
+                    },
+                    getListIOException(ArrayList::new, dataInputStream::readInt, 1).get(0));
             return resume;
         }
     }
@@ -151,6 +119,11 @@ public class DataStreamStorageStrategy implements IOStorageStrategy {
         T get() throws IOException;
     }
 
+    @FunctionalInterface
+    private interface IOExceptionBiSupplier<T, V> {
+        T get(V v) throws IOException;
+    }
+
     private static <T> void forEachConsume(Collection<? extends T> collection, IOExceptionConsumer<T> action) throws IOException {
         for (T t : collection) {
             action.apply(t);
@@ -163,5 +136,18 @@ public class DataStreamStorageStrategy implements IOStorageStrategy {
             collection.add(element.get());
         }
         return collection;
+    }
+
+    private static void addContacts(Resume resume, IOExceptionSupplier<ContactTypes> contactType, IOExceptionSupplier<Contact> contact, int size) throws IOException {
+        for (int i = 0; i < size; i++) {
+            resume.addContact(contactType.get(), contact.get());
+        }
+    }
+
+    private static void addSections(Resume resume, IOExceptionSupplier<SectionTypes> sectionType, IOExceptionBiSupplier<AbstractSection, SectionTypes> section, int size) throws IOException {
+        for (int i = 0; i < size; i++) {
+            SectionTypes type = sectionType.get();
+            resume.addSection(type, section.get(type));
+        }
     }
 }
