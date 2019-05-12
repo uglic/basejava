017db686af30d27898fa118e1580ca26279a5bb3
Stepan Shcherbakov <me@uglic.ru>
2019-05-12 22:33:45 +0300

    Lesson#10: deadlock && livelock samples version 2

--- src/ru/javawebinar/basejava/MainHW11Locks.java
+++ src/ru/javawebinar/basejava/MainHW11Locks.java
@@ -7,7 +7,7 @@ public class MainHW11Locks {
 
         public synchronized void doSmoke(DeadLock lock) {
             try {
-                Thread.currentThread().sleep(10);  // efficiently rises the probability of the deadlock
+                Thread.sleep(10);  // efficiently rises the probability of the deadlock
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -26,25 +26,16 @@ public class MainHW11Locks {
             this.name = name;
         }
 
-        boolean setOwner(LiveLock locker) {
-            if (locker == null) {
-                synchronized (name) {
-                    owner = null;
-                    return true;
-                }
+        synchronized boolean setOwner(LiveLock locker) {
+            if (locker == null || owner == null) {
+                owner = null;
+                return true;
             } else {
-                synchronized (name) {
-                    if (owner == null) {
-                        owner = locker;
-                        return true;
-                    } else {
-                        return false;
-                    }
-                }
+                return false;
             }
         }
 
-        boolean isOwner(LiveLock locker) {
+        synchronized boolean isOwner(LiveLock locker) {
             return (owner == locker);
         }
     }
@@ -68,7 +59,7 @@ public class MainHW11Locks {
                     }
                 }
                 try {
-                    Thread.sleep(200 + (int)(300*Math.random()));
+                    Thread.sleep(1); //+ (int) (3 * Math.random())
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -95,9 +86,70 @@ public class MainHW11Locks {
         }
     }
 
+    static class ResourceObject2 {
+        private final String name;
+        private LiveLock2 owner;
+
+        ResourceObject2(String name) {
+            this.name = name;
+        }
+
+        synchronized boolean setOwner(LiveLock2 locker) {
+            if (locker == null || owner == null) {
+                owner = null;
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        synchronized boolean isOwner(LiveLock2 locker) {
+            return (owner == locker);
+        }
+    }
+
+    static class LiveLock2 {
+        private String name;
+        private boolean isOnDutyToday;
+
+        public LiveLock2(String name) {
+            this.name = name;
+            isOnDutyToday = true;
+        }
+
+        synchronized public boolean isOnDutyToday() {
+            return isOnDutyToday;
+        }
+
+        public void clearDesk() {
+            System.out.println(Thread.currentThread().getName() + ": I cleared the desk");
+        }
+
+        public void beginLesson(ResourceObject2 desk, LiveLock2 helper) {
+            int counter = 0;
+            while (isOnDutyToday) {
+                counter++;
+                if (!desk.isOwner(this)) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (helper.isOnDutyToday()) {
+                    desk.setOwner(helper);
+                    System.out.println(Thread.currentThread().getName() + ": Friends must help us! Try #" + counter);
+                } else {
+                    clearDesk();
+                    isOnDutyToday = false;
+                }
+            }
+        }
+    }
+
     public static void makeDeadLock() {
-        DeadLock lockA = new DeadLock();
-        DeadLock lockB = new DeadLock();
+        final DeadLock lockA = new DeadLock();
+        final DeadLock lockB = new DeadLock();
         Thread thread0 = new Thread(() -> lockA.doSmoke(lockB));
         Thread thread1 = new Thread(() -> lockB.doSmoke(lockA));
         thread0.start();
@@ -115,7 +167,21 @@ public class MainHW11Locks {
         thread1.start();
     }
 
+    public static void makeLiveLock2() {
+        ResourceObject2 desk = new ResourceObject2("Andy");
+        LiveLock2 locker1 = new LiveLock2("Andy");
+        LiveLock2 locker2 = new LiveLock2("Bazz");
+        LiveLock2 locker3 = new LiveLock2("Will");
+        Thread thread0 = new Thread(() -> locker1.beginLesson(desk, locker2));
+        Thread thread1 = new Thread(() -> locker2.beginLesson(desk, locker3));
+        Thread thread2 = new Thread(() -> locker3.beginLesson(desk, locker1));
+        thread0.start();
+        thread1.start();
+        thread2.start();
+    }
+
     public static void main(String[] args) {
+        makeLiveLock2();
         makeDeadLock();
         //makeLiveLock();
     }
