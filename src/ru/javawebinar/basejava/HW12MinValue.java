78305df6885767165e1587d87eaaf5cc8d9cd26e
Stepan Shcherbakov <me@uglic.ru>
2019-05-13 12:11:47 +0300

    Lesson#12: int minValue(int []) in HW12MinValue

---
+++ src/ru/javawebinar/basejava/HW12MinValue.java
@@ -0,0 +1,47 @@
+package ru.javawebinar.basejava;
+
+import java.util.Arrays;
+
+public class HW12MinValue {
+    static int minValue(int[] values) {
+        return Arrays.stream(values).sorted().reduce((s, i) -> {
+            if (i >= 10 || i < 0) return 0;
+            if ((s - i) % 10 != 0) {
+                return s * 10 + i;
+            } else {
+                return s;
+            }
+        }).orElse(0);
+    }
+
+    static String printIntArray(int[] values) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int value : values) {
+            if (sb.length() > 1) sb.append(",");
+            sb.append(value);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        int[] values;
+        int result;
+
+        values = new int[]{1, 2, 3, 3, 2, 3};
+        System.out.println("minValue for " + printIntArray(values) + " is " + minValue(values));
+
+        values = new int[]{10};
+        System.out.println("minValue for " + printIntArray(values) + " is " + minValue(values));
+
+        values = new int[]{0};
+        System.out.println("minValue for " + printIntArray(values) + " is " + minValue(values));
+
+        values = new int[]{};
+        System.out.println("minValue for " + printIntArray(values) + " is " + minValue(values));
+
+        values = new int[]{1, 1, 1, 1, 1, 3, 3, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println("minValue for " + printIntArray(values) + " is " + minValue(values));
+    }
+}
